a7345dbda6eb6100c40fd5288162bd901ef93c37
Aman Raj <160530800+rajamannn@users.noreply.github.com>
2024-03-26 16:29:43 +0530

    Add files via upload

---
+++ Odd and Even number.c
@@ -0,0 +1,18 @@
+//program for odd and even number
+
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the number");
+    scanf("%d",&a);
+if(a%2==0)
+{
+   printf("even");
+}
+else
+{
+   printf("odd");
+}
+return 0;
+}
